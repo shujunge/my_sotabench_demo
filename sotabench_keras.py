4e6034beff055e58b8a745cee8d2d9107d3e1ca8
shujunge <zfwang@stu.xidian.edu.cn>
2019-11-26 00:09:52 +0800

    submited efficientb4

--- sotabench_keras.py
+++ sotabench_keras.py
@@ -16,7 +16,9 @@ model_name = 'efficientnet-b4'
 model = EfficientNet.from_pretrained(model_name)
 
 input_transform = transforms.Compose([
-    transforms.Resize(224), transforms.ToTensor(),
+    transforms.Resize(256, PIL.Image.BICUBIC),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
     transforms.Normalize(
     mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
